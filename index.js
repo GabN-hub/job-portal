2383ceec0c8705cb043083341f55a6e5c9388c76
Gab Neri <gabriella_neri@gmail.com>
2023-07-31 21:50:59 +0800

    boiler template

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+
+const app = express();
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("layout", "./layouts/master");
+
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+app.listen(3000);
+console.log("Server is running");
